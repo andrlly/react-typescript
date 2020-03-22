860549927a04b67319f5ccbcea3bce65e53e4769
Andrii Venhlinskyi <avenhlinskyi@mains-MacBook-Pro-4.local>
2020-03-22 22:48:42 +0200

    first commit

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,39 @@
+import React, {useRef} from 'react';
+
+interface TodoFormProps {
+    onAdd(title: string): void,
+}
+
+export const TodoForm: React.FC<TodoFormProps> = props => {
+    // const [title, setTitle] = useState<string>('');
+    const ref = useRef<HTMLInputElement>(null);
+
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value)
+    // };
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter' && ref.current!.value !== '') {
+            props.onAdd(ref.current!.value);
+            ref.current!.value = '';
+            //     setTitle('');
+        }
+    };
+
+    return (
+        <div className="input-field mt2">
+            <input
+                // onChange={changeHandler}
+                // value={title}
+                ref={ref}
+                type="text"
+                id="title"
+                placeholder="Enter todo name"
+                onKeyPress={keyPressHandler}
+            />
+            <label htmlFor="title" className="active">
+                Enter todo name
+            </label>
+        </div>
+    )
+};
